d40f9a37e50d2eb78ac6631da54c9e6650cc8db2
Rafael Martins <139396742+Rafamsa@users.noreply.github.com>
2026-01-29 09:52:13 -0300

    created list users request

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("Hello world!")
+import requests
+
+def list_users():
+    response = requests.get("https://697b4b380e6ff62c3c5b9ac0.mockapi.io/api/v1/users")
+    return response.json()
+
+print(list_users())
