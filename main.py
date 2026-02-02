e1e4d1a607e1102e2bec8724097cc387cec5e22b
Rafael Martins <139396742+Rafamsa@users.noreply.github.com>
2026-02-02 09:55:13 -0300

    created user class

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 import requests
 
-def list_users():
-    response = requests.get("https://697b4b380e6ff62c3c5b9ac0.mockapi.io/api/v1/users")
-    return response.json()
+class User: #"receita de objetos"
+    def list(self):
+        response = requests.get("https://697b4b380e6ff62c3c5b9ac0.mockapi.io/api/v1/users")
+        return response.json()
 
-print(list_users())
+    def get(self):
+        raise NotImplementedError
+    
+    def save(self):
+        raise NotImplementedError
+
+user = User() #instanciar uma classe, deixa de ser uma classe e passa a ser um objeto
+
+print(user.list())
