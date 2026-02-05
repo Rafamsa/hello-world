79b257b501fbe8e9b7fd8b2bad466f7c51cb428f
Rafael Martins <139396742+Rafamsa@users.noreply.github.com>
2026-02-05 12:14:31 -0300

    modify table sql

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
 import requests
+import psycopg
 
-class User: #"receita de objetos"
+DATABASE = {
+    "name": "postgres",
+    "user": "postgres",
+    "password": "postgres",
+    "host": "localhost",
+    "port": "5432",
+}  # constante, por isso letra maiuscula
+
+
+def get_connection(**options):
+    conninfo = f"dbname={options['name']} user={options['user']} password={options['password']} host={options['host']} port={options['port']}"
+    return psycopg.connect(conninfo)
+
+
+class User:  # "receita de objetos"
     def list(self):
-        response = requests.get("https://697b4b380e6ff62c3c5b9ac0.mockapi.io/api/v1/users")
+        response = requests.get(
+            "https://697b4b380e6ff62c3c5b9ac0.mockapi.io/api/v1/users"
+        )
         return response.json()
 
     def get(self):
         raise NotImplementedError
-    
+
     def save(self):
         raise NotImplementedError
 
-user = User() #instanciar uma classe, deixa de ser uma classe e passa a ser um objeto
 
-print(user.list())
+get_connection(**DATABASE)
