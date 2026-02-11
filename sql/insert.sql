27e3bba0529a3b19add879f9855107226dc12ab7
Rafael Martins <139396742+Rafamsa@users.noreply.github.com>
2026-02-11 10:21:45 -0300

    create insert sql

---
+++ sql/insert.sql
@@ -0,0 +1,6 @@
+INSERT INTO users(name, avatar, email)
+VALUES(
+    'Katara', 
+    'https://kanto.legiaodosherois.com.br/w760-h398-cfill/wp-content/uploads/2021/10/legiao_G0qPNIOR_QeT.png.webp', 
+    'katara.katara@yahoo.com'
+)
